f1d41566a3de2039c1908cea7eebdf13d7680cc4
Karin-518 <karin.410@ezweb.ne.jp>
2025-08-29 11:35:52 +0900

    プレイヤー、ゲームシーンクラス追加

---
+++ SweetHex Café/Src/Object/Player.cpp
@@ -0,0 +1,25 @@
+#include "Player.h"
+
+Player::Player(void)
+{
+}
+
+Player::~Player(void)
+{
+}
+
+void Player::Init(void)
+{
+}
+
+void Player::Update(void)
+{
+}
+
+void Player::Draw(void)
+{
+}
+
+void Player::Release(void)
+{
+}
---
+++ SweetHex Café/Src/Object/Player.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class Player
+{
+public:
+	Player(void);
+	~Player(void);
+
+	void Init(void);
+	void Update(void);
+	void Draw(void);
+	void Release(void);
+
+private:
+};
+
